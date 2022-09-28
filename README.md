e051de15c9f6fd2e327696aee942ac46655cf88a
Mia <113086524+MiaYun98@users.noreply.github.com>
2022-09-27 20:47:15 -0700

    fin playing around with the JavaScript Code

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # password-Generator
+
+## Description
+
+It is more like Algorithm practice
+
+This is the first project I've done after learning about the JavaScript
+
+I used the if and for loop to make the program work properly 
+
+Also used the prompt and comfirm for the interaction with the user 
+
+## User Story 
+
+```
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+## Languages
+
+English
+
+HTML, CSS, & JavaScript
+
+## Usage
+
+To generate random password with the criteria that user wants. 
+
+Such as length, number, alphabet, upper, lower, and some special symbols.
+
+People can generate the password that they want but the selection of page alart. 
+
+## App Screenshot
+
+![](Assets/03-javascript-homework-demo.png)
+
+## Live website
+
+![](Assets/Untitled.png)
+
+## Contact
+
+Linkedin: https://www.linkedin.com/in/sangmi-yun-0a4241201/
---
+++ script.js
@@ -0,0 +1,90 @@
+// Assignment Code
+function generatePassword() {
+
+  //variables needed for the Math  
+  var selectedWord = "";
+  var needWord = "";
+  var section = 0;
+
+  // gets user input more than 8 less than 128 characters 
+  var numwords = prompt("How many words you need for the password?");    
+  if (8 <= numwords && numwords <= 126) {
+
+    // asking if yes to the lowercase 
+    var lowercase = confirm("Do you want lowecase in your password?")
+    if (lowercase) {
+      var alphabetLower = "abcdefghijklmnopqrstuvwxyz";
+      selectedWord = selectedWord + alphabetLower;
+      //at least one 
+      needWord = needWord + alphabetLower[Math.floor(Math.random() * alphabetLower.length)];
+      section = section + 1;
+    selectedWord
+      selectedWord = selectedWord + "";
+      needWord = needWord + "";
+    }
+
+    // if yes to the Uppercase 
+    var uppercase = confirm("Do you want Uppercase in your password?");
+    if (uppercase) {
+      var alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      selectedWord = selectedWord + alphabetUpper;
+      //at least one 
+      needWord = needWord + alphabetUpper[Math.floor(Math.random() * alphabetUpper.length)];
+      section = section + 1;
+    } else { 
+      selectedWord = selectedWord + "";
+    }
+
+    // if yes to the numeric
+    var numeric = confirm("Do you want Numbers in your password?");
+    if (numeric) {
+      var number = "0123456789";
+      selectedWord = selectedWord + number;
+      //at least one 
+      needWord = needWord + number[Math.floor(Math.random() * number.length)];
+      section = section + 1;
+    } else { 
+      selectedWord = selectedWord + "";
+    }
+
+    // if yes to the special characters
+    var special = confirm("Do you want special characters in your password?");
+    if (special) {
+      var characters="!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+      selectedWord = selectedWord + characters;
+      //at least one 
+      needWord = needWord + characters[Math.floor(Math.random() * characters.length)];
+      section = section + 1;
+    } else { 
+      selectedWord = selectedWord + "";
+    }
+
+    // if the answer is valid with at least one character letter 
+    if (selectedWord.length == 0) {  
+      password = "You need to choose at least one element.";
+    } else {
+      //choosing the number from the selectedWord (list)
+      for (var i = 1; i <= (numwords-section); i++) {
+        needWord = needWord + selectedWord[Math.floor(Math.random() * selectedWord.length)];
+      }
+      //randomize the number
+      password = needWord.split('').sort(function(){return 0.5-Math.random()}).join('');
+    }
+  } else {
+    //if the numer is not between 9 - 128
+    password = "Please choose the number between 8 to 128.";
+  }
+  return password;
+}
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
